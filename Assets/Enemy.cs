50a6972bc5f01c9c15de7ddb53798b524b1fcce8
MechaChaz <kortelainen.jaakko@gmail.com>
2020-05-08 19:14:35 +0300

    Player controls commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int hP = 100;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+
+        
+    }
+    void Dead()
+    {
+        //kaikenlaista animaatio paskaa
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        hP -= damage;
+        if (hP <= 0)
+        {
+            Dead();
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] int damage = 50;
+    Rigidbody2D rb;
+    Vector2 movement;
+    Enemy enemy;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Hitting();
+        }
+    }
+
+
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    void Hitting()
+    {
+       //Animaatio pasket tänne
+    }
+}
